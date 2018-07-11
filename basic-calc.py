eec92b4762f3ac8d55d0be843f47e8853a1e2b45
Schneider <dschneider@amphi.com>
2018-07-10 20:56:43 -0700

    Added basic-calc.py starter code

---
+++ basic-calc.py
@@ -0,0 +1,42 @@
+#Computer Scientist:
+#Date:
+
+#################
+# TODO: Ask the user for 2 numbers
+#################
+
+
+
+#################
+# TODO: Add the numbers and display the result
+#################
+
+
+
+#################
+# TODO: Subtract the numbers and display the result
+#################
+
+
+
+#################
+# TODO: Multiply the numbers and display the result
+#################
+
+
+
+#################
+# TODO: Divide the numbers and display the result
+#################
+
+
+
+#################
+# TODO: Exponent the numbers and display the result
+#################
+
+
+
+#################
+# TODO: Modulus the numbers and display the result
+#################
