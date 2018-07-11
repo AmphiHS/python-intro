d652867ebe291f8655606a3f794dbd6f5aa56c56
Schneider <dschneider@amphi.com>
2018-07-10 20:48:34 -0700

    made student questionnaire template

---
+++ student.py
@@ -0,0 +1,22 @@
+#Computer Scientist:
+#Date:
+
+#################
+# TODO: Ask The User for their Name, Age, and Safari Animal
+#################
+
+
+
+
+
+#################
+# TODO: Ask the user 3 questions of my choosing
+#################
+
+
+
+
+
+#################
+# TODO: Print a positive message with the users name
+#################
