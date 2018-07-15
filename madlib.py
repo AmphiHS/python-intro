490b2638ccb50ad5e321907c3571bf688ddf59ac
Schneider <dschneider@amphi.com>
2018-07-14 18:49:28 -0700

    Added madlib.py

---
+++ madlib.py
@@ -0,0 +1,60 @@
+#Computer Scientist:
+#Date:
+
+#################
+# TODO: Ask The use for their 6 nouns
+#################
+
+
+
+
+
+
+#################
+# TODO: Ask The use for their 3 verbs
+#################
+
+
+
+
+
+#################
+# TODO: Ask The use for their 2 adjectives
+#################
+
+
+
+
+
+
+#################
+# TODO: Ask The use for their 2 adverbs
+#################
+
+
+
+
+
+
+#################
+# TODO: Ask The use for a location
+#################
+
+
+
+
+
+
+
+#################
+# TODO: Ask The use for a color
+#################
+
+
+
+
+
+
+#################
+# TODO: Print out your story all on a single line
+#################
